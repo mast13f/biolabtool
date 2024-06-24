f9151e53bcadbe0fa4d63019f2437b84c2db64bd
Peiyang Liu <nickpliu@bu.edu>
2024-06-24 13:44:50 -0400

    add user input

--- volcano_plot.py
+++ volcano_plot.py
@@ -1,5 +1,6 @@
 
 import pandas as pd
+import matplotlib
 import matplotlib.pyplot as plt
 import seaborn as sns
 
@@ -8,31 +9,73 @@ import seaborn as sns
 # more here: https://resources.qiagenbioinformatics.com/manuals/rnaanalysisportal/current/index.php?manual=Volcano_plot.html
 
 # user input
-x = input('x:')
-y = input('y:')
-name = input('name:')
-fc_threshold_lower = float(input('fc_threshold_lower:'))
-fc_threshold_upper = float(input('fc_threshold_upper:'))
-sig_threshold = float(input('sig_threshold:'))
+# x = input('x:')
+# y = input('y:')
+# name = input('name:')
+# fc_threshold_lower = float(input('fc_threshold_lower:'))
+# fc_threshold_upper = float(input('fc_threshold_upper:'))
+# sig_threshold = float(input('sig_threshold:'))
 
-def volcano_plot(data, x, y, name, fc_threshold_lower, fc_threshold_upper, sig_threshold):
-    x_axis = x
-    y_axis = y
-    threshold = sig_threshold
-    fc_threshold_lower = fc_threshold_lower
-    fc_threshold_upper = fc_threshold_upper
-    plt.scatter(x=data[x_axis], y=data[y_axis], s=1, label="Not significant")
+# user input - for testing only
+x = 'Log2FC'
+y = 'minuslog10(pval)'
+name = 'Name'
+fc_threshold_lower = -0.5
+fc_threshold_upper = 0.5
+sig_threshold = 0.5
+data = pd.read_csv('test.csv')
 
-    down = data[(data[x_axis] <= fc_threshold_upper) & (data[y_axis] <= threshold)]
-    up = data[(data[x_axis] >= fc_threshold_lower) & (data[y_axis] <= threshold)]
+### settings
 
-    plt.scatter(x=down[x_axis], y=down[y_axis], s=3, label="Down-regulated", color="blue")
-    plt.scatter(x=up[x_axis], y=up[y_axis], s=3, label="Up-regulated", color="red")
+# show labels?
+show_labels = input('Show labels? (y/n):')
+if show_labels == 'y':
+    show_labels = True
+else:
+    show_labels = False
 
-    plt.xlabel(x_axis)
-    plt.ylabel(y_axis)
-    plt.axvline(-threshold, color="grey", linestyle="--")
-    plt.axvline(threshold, color="grey", linestyle="--")
-    plt.axhline(threshold, color="grey", linestyle="--")
-    plt.legend()
+# colors?
+# ns_color = input('Not significant color:')
+# ur_color = input('Up-regulated color:')
+# dr_color = input('Down-regulated color:')
+
+# user input - for testing only
+ns_color = 'lightgrey'
+ur_color = 'deepskyblue'
+dr_color = 'orangered'
+
+
+
+def volcano_plot(data, x, y, name, fc_threshold_lower, fc_threshold_upper, sig_threshold, show_labels, ns_color, ur_color, dr_color, mark_all=False ):
+    
+    # not significant
+    plt.scatter(x=data[x], y=data[y], s=1, label="Not significant", color=ns_color)
+
+    # highlight down- or up- regulated genes
+    down = data[(data[x] >= fc_threshold_upper) & (data[y] >= sig_threshold)]
+    up = data[(data[x] <= fc_threshold_lower) & (data[y] >= sig_threshold)]
+    plt.scatter(x=down[x], y=down[y], s=3, label="Up-regulated", color=ur_color)
+    plt.scatter(x=up[x], y=up[y], s=3, label="Down-regulated", color=dr_color)
+
+    # mark the geno name of points of interest in the graph
+    if mark_all:
+        for i in range(len(data)):
+            if data[x][i] >= fc_threshold_upper and data[y][i] >= sig_threshold:
+                plt.text(data[x][i], data[y][i], data[name][i], fontsize=8, color='black')
+            elif data[x][i] <= fc_threshold_lower and data[y][i] >= sig_threshold:
+                plt.text(data[x][i], data[y][i], data[name][i], fontsize=8, color='black')
+    
+    # 
+
+
+
+    plt.xlabel(x)
+    plt.ylabel(y)
+    plt.axvline(fc_threshold_lower, color="grey", linestyle="--")
+    plt.axvline(fc_threshold_upper, color="grey", linestyle="--")
+    plt.axhline(sig_threshold, color="grey", linestyle="--")
+    if show_labels:
+        plt.legend()
     plt.show()
+
+volcano_plot(data, x, y, name, fc_threshold_lower, fc_threshold_upper, sig_threshold, show_labels, ns_color, ur_color, dr_color, mark_all=True)
