25fa481275600fe661abf57ee55aaf6ffecf80e6
Peiyang Liu <nickpliu@bu.edu>
2024-06-26 12:48:41 -0400

    gui

--- vocano_plot.py
+++ vocano_plot.py
@@ -2,37 +2,38 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 from adjustText import adjust_text
+import user_input_gui
 
-def get_user_input():
-    # User input - for testing only, replace with actual user input as needed
-    x = 'Log2FC'
-    y = 'minuslog10(pval)'
-    name = 'Name'
-    fc_threshold_lower = -0.5
-    fc_threshold_upper = 0.5
-    sig_threshold = 0.5
-    show_labels = False
-    ns_color = 'lightgrey'
-    ur_color = 'deepskyblue'
-    dr_color = 'orangered'
-    mark_all = False
+# def get_user_input():
+#     # User input - for testing only, replace with actual user input as needed
+#     data = pd.read_csv('test.csv') # select from drive
+#     x = 'Log2FC' # select from columns
+#     y = 'minuslog10(pval)' # select from columns
+#     name = 'Name' # sekect from columns
+#     fc_threshold_lower = -1 # user input
+#     fc_threshold_upper = 1 # user input
+#     sig_threshold = 0.5 # user input
+#     show_labels = False # check box
+#     ns_color = 'lightgrey' # select from color picker
+#     ur_color = 'deepskyblue' # select from color picker
+#     dr_color = 'orangered' # select from color picker
+#     mark_all = True # check box
     
-    # Points of interest input
-    pofi_input = input('Points of interest? (y/n):')
-    if pofi_input == 'y':
-        pofi = input('Enter the name of points of interest (comma separated):').split(',')
-    else:
-        pofi = []
+#     # Points of interest input
+#     pofi = input('Enter the name of points of interest (comma separated):').split(', ')  # user input
     
-    return (x, y, name, fc_threshold_lower, fc_threshold_upper, sig_threshold, show_labels, ns_color, ur_color, dr_color, mark_all, pofi)
+#     return (data, x, y, name, fc_threshold_lower, fc_threshold_upper, sig_threshold, show_labels, ns_color, ur_color, dr_color, mark_all, pofi)
 
 def check_pofi_in_data(pofi, data, name):
     valid_pofi = []
-    for p in pofi:
-        if p in data[name].values:
-            valid_pofi.append(p)
-        else:
-            print(f'{p} not found in the data')
+    if not pofi:
+        print("No points of interest selected")
+    else:
+        for p in pofi:
+            if p in data[name].values:
+                valid_pofi.append(p)
+            else:
+                print(f'{p} not found in the data')
     return valid_pofi
 
 def plot_volcano(data, x, y, name, fc_threshold_lower, fc_threshold_upper, sig_threshold, show_labels, ns_color, ur_color, dr_color, mark_all, pofi):
@@ -53,7 +54,7 @@ def plot_volcano(data, x, y, name, fc_threshold_lower, fc_threshold_upper, sig_t
                 texts.append(plt.text(data[x][i], data[y][i], data[name][i], fontsize=8, color='black'))
         if data[name][i] in pofi:
             texts.append(plt.text(data[x][i], data[y][i], data[name][i], fontsize=8))
-    adjust_text(texts)
+    adjust_text(texts, arrowprops=dict(arrowstyle="-", color='black', lw=0.5))
 
     plt.xlabel(x)
     plt.ylabel(y)
@@ -65,11 +66,23 @@ def plot_volcano(data, x, y, name, fc_threshold_lower, fc_threshold_upper, sig_t
     plt.show()
 
 def main():
-    data = pd.read_csv('test.csv')
-    user_input = get_user_input()
-    x, y, name, fc_threshold_lower, fc_threshold_upper, sig_threshold, show_labels, ns_color, ur_color, dr_color, mark_all, pofi = user_input
+    user_inputs = user_input_gui.get_user_input()
+    data = user_inputs['data']
+    x = user_inputs['x']
+    y = user_inputs['y']
+    name = user_inputs['name']
+    fc_threshold_lower = user_inputs['fc_threshold_lower']
+    fc_threshold_upper = user_inputs['fc_threshold_upper']
+    sig_threshold = user_inputs['sig_threshold']
+    show_labels = user_inputs['show_labels']
+    ns_color = user_inputs['ns_color']
+    ur_color = user_inputs['ur_color']
+    dr_color = user_inputs['dr_color']
+    mark_all = user_inputs['mark_all']
+    pofi = user_inputs['pofi']
     valid_pofi = check_pofi_in_data(pofi, data, name)
     plot_volcano(data, x, y, name, fc_threshold_lower, fc_threshold_upper, sig_threshold, show_labels, ns_color, ur_color, dr_color, mark_all, valid_pofi)
 
 if __name__ == "__main__":
     main()
+
